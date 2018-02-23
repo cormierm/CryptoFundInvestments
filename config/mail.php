1adb2d99241d71678a06e73903482618aa2c3347
Matt C <cormierm@gmail.com>
2018-02-23 14:10:34 -0500

    Added mail support for forget password

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'support@capstone.mattcormier.com'),
+        'name' => env('MAIL_FROM_NAME', 'Crypto Fund Investments'),
     ],
 
     /*
