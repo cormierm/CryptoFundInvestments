028d9fb1d3d4c0179fce3f7a3a2a280f90697f88
Matt Cormier <cormierm@gmail.com>
2018-03-14 10:07:00 -0400

    added missing files for layout and styling

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,24 @@
+<!-- Created: 3/8/2018
+    by: Lucas Kaastra
+    Modified:
+ -->
+
+    <!-- Styles -->
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+
+<div class="dashboardsidebar">
+    {{--<img src="#" alt="Profile Picture">--}}
+    <a href="/profile">Profile</a>
+    <a href="/dashboard">Dashboard</a>
+    <a href="/funds">Funds</a>
+    <a href="#">Coin Lookup</a>
+    <a href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+            Logout
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+</div>
