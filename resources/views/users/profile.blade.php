51c5dcef72ff028e14fb234aec89da83b1922759
Matt Cormier <cormierm@gmail.com>
2018-04-12 13:20:35 -0400

    Updated view

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -15,11 +15,25 @@
                     <br>
                 @endif
                 <div class="card card-default">
-                    <div class="card-header">{{ $user->first_name }}'s Profile</div>
-                        <div class="card-body">
+                    <div class="card-header">
+                        <h3>
+                            {{ $user->first_name }}'s Profile
                             @if($user->id == Auth::user()->getAuthIdentifier())
                                 <a href="/profile/edit"><button class="btn btn-primary float-right">Edit Profile</button></a>
                             @endif
+                            @if (!$user->isTrader() && $user->id == Auth::user()->getAuthIdentifier())
+                                <form method="post" action="/profile/requestTraderRole" class="float-right">
+                                    @csrf
+                                    <input type="submit" class="btn btn-danger" value="Request to be Trader" />
+                                </form>
+                            @elseif ($user->id == Auth::user()->getAuthIdentifier())
+                                <a href="/profile/remove_trader_role"><button class="btn btn-danger float-right">Remove trader role</button></a>
+                            @endif
+
+                        </h3>
+                    </div>
+                        <div class="card-body">
+
                         <p><strong>Email:</strong> {{ $user->email }}</p>
                         <p><strong>First Name:</strong> {{ $user->first_name }}</p>
                         <p><strong>Last Name:</strong> {{ $user->last_name }}</p>
@@ -27,14 +41,7 @@
 
 
 
-                        @if (!$user->isTrader() && $user->id == Auth::user()->getAuthIdentifier())
-                            <form method="post" action="/profile/requestTraderRole">
-                                @csrf
-                                <input type="submit" class="btn btn-primary" value="Request to be Trader" />
-                            </form>
-                        @elseif ($user->id == Auth::user()->getAuthIdentifier())
-                            <a href="/profile/remove_trader_role"><button class="btn btn-danger">Remove trader role</button></a>
-                        @endif
+
 
                         </div>
                 </div>
